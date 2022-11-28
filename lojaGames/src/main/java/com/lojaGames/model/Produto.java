8339d8dd1d2f739a3c2979ea5de952932a6d9a89
Willian Lima <willian.slima@mercadolivre.com>
2022-11-28 15:59:42 -0300

    Create projeto

---
+++ lojaGames/src/main/java/com/lojaGames/model/Produto.java
@@ -0,0 +1,15 @@
+package com.lojaGames.model;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+public class Produto {
+	
+	private Long id;
+	private String produto;
+	private BigDecimal preco;
+	private List<Categoria> categoria;
+	
+	
+
+}
---
+++ lojaGames/src/test/java/com/lojaGames/LojaGamesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lojaGames;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaGamesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
