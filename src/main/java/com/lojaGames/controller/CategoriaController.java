80df1e41f297d17fdf30a03ea93407a087b1eb59
Willian Lima <willian.slima@mercadolivre.com>
2022-11-29 14:06:18 -0300

    CRUD OK - BuscaPreco > e < OK

--- src/main/java/com/lojaGames/controller/CategoriaController.java
+++ src/main/java/com/lojaGames/controller/CategoriaController.java
@@ -1,4 +1,4 @@
-package com.lojaGames.Controller;
+package com.lojaGames.controller;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/com/lojaGames/controller/ProdutoController.java
+++ src/main/java/com/lojaGames/controller/ProdutoController.java
@@ -1,4 +1,4 @@
-package com.lojaGames.Controller;
+package com.lojaGames.controller;
 
 import java.math.BigDecimal;
 import java.util.List;
